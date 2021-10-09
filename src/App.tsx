cc9f15405a429ce748305379825a16b6da4be81e
Bryam Rodriguez <brodriguezaybar>
2021-10-09 13:35:13 -0500

    Add styled-components and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './config/theme';
 
 function App() {
+  const [theme] = useState<'light' | 'dark'>('dark');
+
+  // const themeToggler = () => {
+  //   theme === 'dark' ? setTheme('light') : setTheme('dark');
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/config/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  body: '#FFF',
+  text: '#363537',
+  background: '#363537',
+};
+
+export const darkTheme: DefaultTheme = {
+  body: '#363537',
+  text: '#FAFAFA',
+  background: '#999',
+};
